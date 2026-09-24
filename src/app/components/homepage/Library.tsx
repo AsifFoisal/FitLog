8da17600d3cf6d722a2c05ba77d63b4c0a391016
Asif9060 <moodyfoisal@gmail.com>
2026-09-25 01:49:00 +0600

    Finished Library Section

--- src/app/components/homepage/Library.tsx
+++ src/app/components/homepage/Library.tsx
@@ -1,21 +1,27 @@
 import { IWorkout } from "@/types/workout.type";
 import WorkoutCard from "./WorkoutCard";
 
-const getWorkout = async() => {
+const getWorkout = async () => {
     const res = await fetch("https://api.abcz.workers.dev/api/fitlog");
     const data = res.json();
     return data;
 }
-const Library = async() => {
+const Library = async () => {
 
     const workoutData = await getWorkout();
     return (
-        <div className=" max-w-7xl mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-            {
-                workoutData.map((workout: IWorkout) => {
-                    return <WorkoutCard key={workout.id} workout={workout}/>
-                })
-            }
+        <div className="max-w-7xl mx-auto">
+            <div className="mt-30">
+                <h1 className="text-[30px] font-oswald font-bold tracking-[-0.75px]">THE LIBRARY</h1>
+                <p className="text-[#9CA3AF] text-[14px]">Twelve lifts covering every major muscle group.</p>
+            </div>
+            <div className="  grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mt-[32px]">
+                {
+                    workoutData.map((workout: IWorkout) => {
+                        return <WorkoutCard key={workout.id} workout={workout} />
+                    })
+                }
+            </div>
         </div>
     );
 };
