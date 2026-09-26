2981f9b17ecd80634a11176d373272c8c89d4077
Asif9060 <moodyfoisal@gmail.com>
2026-09-26 16:00:00 +0600

    Fixed Some Ui Bugs

--- src/app/components/homepage/Library.tsx
+++ src/app/components/homepage/Library.tsx
@@ -10,7 +10,7 @@ const Library = async () => {
 
     const workoutData = await getWorkout();
     return (
-        <div id="library" className="max-w-7xl mx-auto mb-16">
+        <div id="library" className="max-w-7xl mx-auto mb-10 sm:mb-12 lg:mb-16 px-4 sm:px-6 lg:px-8 xl:px-0">
             <div className="mt-16 sm:mt-20 md:mt-24 lg:mt-30 lg:text-left md:text-left px-4 sm:px-0 text-center">
                 <h1 className="text-[26px] sm:text-[28px] md:text-[30px] font-oswald font-bold tracking-[-0.5px] sm:tracking-[-0.75px]">
                     THE LIBRARY
@@ -20,7 +20,7 @@ const Library = async () => {
                     Twelve lifts covering every major muscle group.
                 </p>
             </div>
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mt-8">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-8">
                 {
                     workoutData.map((workout: IWorkout) => {
                         return <WorkoutCard key={workout.id} workout={workout} />
